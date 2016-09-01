c20e9bad2ee9e7bc2d2650e2c094af4dde873188
Scott G. Daniel <scottdaniel25@gmail.com>
2016-09-01 13:42:34 -0700

    getting bfrag to work now

---
+++ scripts/03-bowtie2-bfrag.sh
@@ -0,0 +1,46 @@
+#
+# to map the bfrag
+#
+unset module
+source ./config.sh
+
+CWD=$(pwd)
+PROG=`basename $0 ".sh"`
+STDOUT_DIR="$CWD/out/$PROG"
+
+init_dir "$STDOUT_DIR" "$BFRAG_OUT"
+
+cd "$FASTQ_DIR"
+
+for i in $SAMPLE_NAMES; do
+    
+    export SAMPLE=$i
+    export LEFT_FASTQ="$TEMP_DIR/$i-left-fastq"
+    export RIGHT_FASTQ="$TEMP_DIR/$i-right-fastq"
+    export UNPAIRED="$TEMP_DIR/$i-unpaired-fastq"
+
+    #have to do the weird translate and sed stuff
+    #because tophat likes everything comma delimited
+
+#DON'T HAVE TO DO THIS EVERYTIME IF FILES ALREADY THERE
+#    find . -type f -regextype 'sed' -iregex "\.\/$i.*\.1.fastq" \
+#        | sort | tr '\n' ',' | sed "s/,$//g" > $LEFT_FASTQ
+    
+#    find . -type f -regextype 'sed' -iregex "\.\/$i.*\.2.fastq"  \
+#        | sort | tr '\n' ',' | sed "s/,$//g" > $RIGHT_FASTQ
+    
+#    find . -type f -regextype 'sed' -iregex "\.\/$i.*nomatch.*" \
+#        | tr '\n' ',' | sed "s/,$//g" > $UNPAIRED
+
+    echo "Mapping $i FASTQs to $BFRAGBT2"
+
+    JOB=$(qsub -V -N tophatm -j oe -o "$STDOUT_DIR" $WORKER_DIR/run-bowtie2.sh)
+
+    if [ $? -eq 0 ]; then
+        echo Submitted job \"$JOB\" for you. Weeeeeeeeeeeee!
+    else
+        echo -e "\nError submitting job\n$JOB\n"
+    fi
+
+done
+
