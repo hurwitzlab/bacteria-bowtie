3980fbdc4603a38316c40354b4f4b60306f6d1c8
Scott Daniel <scottdaniel25@gmail.com>
2016-09-05 13:09:02 -0700

    and again for mouse

--- scripts/R-interactive/diff_exp_analys_mouse.R
+++ scripts/R-interactive/diff_exp_analys_mouse.R
@@ -1,8 +1,8 @@
 setwd("/Users/Scott/tophat-bacteria/scripts/R-interactive")
 
-system(command = "./process.sh")
+system(command = "./process-mouse.sh")
 
-setwd("/Users/Scott/Google Drive/Hurwitz Lab/cuffdiff_usualsuspects")
+setwd("/Users/Scott/Google Drive/Hurwitz Lab/cuffdiff_mouse_v1/")
 #
 # library(cummeRbund)
 #
@@ -17,19 +17,16 @@ gene_diff <- gene_diff[order(gene_diff$gene,decreasing = T),]
 gene_counts <- read.table("isoforms.fpkm_tracking",header = T)
 gene_counts <- gene_counts[order(gene_counts$gene_short_name,decreasing = T),]
 
-simple_gene_counts<-gene_counts[,c("tracking_id","S1_FPKM","S2_FPKM","S3_FPKM","S4_FPKM")]
+simple_gene_counts<-gene_counts[,c("tracking_id","X.S1_FPKM","S2_FPKM","S3_FPKM","S4_FPKM")]
+colnames(simple_gene_counts)<-c("tracking_id","S1_FPKM","S2_FPKM","S3_FPKM","S4_FPKM")
 simple_gene_counts$sum<-rowSums(simple_gene_counts[,c("S1_FPKM","S2_FPKM","S3_FPKM","S4_FPKM")])
 filtered<-simple_gene_counts[simple_gene_counts$sum!=0,]
 filtered<-filtered[order(filtered$sum,decreasing = T),]
 
-annotation<-read.table("id_to_product.tab",header = F,sep = '\t',quote = "")
-colnames(annotation)<-c("tracking_id","product_name")
-
 gene_annotation<-read.table("id_to_gene.tab",header = F,sep = '\t',quote = "")
 colnames(gene_annotation)<-c("tracking_id","gene")
 
-filtered_annotated<-merge(filtered,annotation,by="tracking_id")
-filtered_annotated<-merge(filtered_annotated,gene_annotation,by="tracking_id",all = F)
+filtered_annotated<-merge(filtered,gene_annotation,by="tracking_id",all = F)
 filtered_annotated<-filtered_annotated[order(filtered_annotated$sum,decreasing = T),]
 
-write.csv(filtered_annotated,"diff_exp_for_all_bact.csv")
+write.csv(filtered_annotated,"diff_exp_for_mouse.csv")
--- scripts/R-interactive/process-mouse.sh
+++ scripts/R-interactive/process-mouse.sh
@@ -1,22 +1,17 @@
 #!/usr/bin/env bash
 
-if [[ -e ./mini-config.sh ]]; then
-    source ./mini-config.sh
+if [[ -e ./mini-config-mouse.sh ]]; then
+    source ./mini-config-mouse.sh
 else
-    echo "no config"
+    echo "no config-mouse"
     exit 1
 fi
-
-echo "Making id_to_product.tab [description]"
-sleep 1
-
-perl -nle '($id)=/ID=([^;]*)/; ($prod)=/product=([^;]*)/; print "$id\t$prod"' \
-    $RWORK_DIR/all_fixed.gff > $RWORK_DIR/id_to_product.tab
+#Y          RefSeq  exon    2397909 2397997 .       +       .       gene_id "Gm3376"; gene_name "Gm3376"; p_id "P8059"; transcript_id "XM_001475806.2"; tss_id "TSS18259";
 
 echo "Making id_to_gene.tab"
 sleep 1
 
-perl -nle '($id)=/ID=([^;]*)/; ($gene)=/gene=([^;]*)/; print "$id\t$gene"' \
-    $RWORK_DIR/all_fixed.gff > $RWORK_DIR/id_to_gene.tab
+perl -nle '($id)=/transcript_id "([^"]*)/; ($gene)=/gene_name "([^"]*)/; print "$id\t$gene"' \
+    $RWORK_DIR/mouse-exons.gtf > $RWORK_DIR/id_to_gene.tab
     
 echo "Leaving directory"
