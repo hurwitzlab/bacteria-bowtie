38569466d7eb36a409f4702fde0eecd80165130d
Scott Daniel <scottdaniel25@gmail.com>
2016-09-05 09:54:05 -0700

    woot, done for all_bact

--- scripts/R-interactive/cummerbund analysis.R
+++ scripts/R-interactive/cummerbund analysis.R
@@ -17,13 +17,18 @@ gene_diff <- gene_diff[order(gene_diff$gene,decreasing = T),]
 gene_counts <- read.table("isoforms.fpkm_tracking",header = T)
 gene_counts <- gene_counts[order(gene_counts$gene_short_name,decreasing = T),]
 
-simple_gene_counts<-gene_counts[,c("tracking_id","gene_short_name","S1_FPKM","S2_FPKM","S3_FPKM","S4_FPKM")]
-simple_gene_counts$sum<-rowSums(simple_gene_counts[,c(3,4,5,6)])
+simple_gene_counts<-gene_counts[,c("tracking_id","S1_FPKM","S2_FPKM","S3_FPKM","S4_FPKM")]
+simple_gene_counts$sum<-rowSums(simple_gene_counts[,c("S1_FPKM","S2_FPKM","S3_FPKM","S4_FPKM")])
 filtered<-simple_gene_counts[simple_gene_counts$sum!=0,]
 filtered<-filtered[order(filtered$sum,decreasing = T),]
 
 annotation<-read.table("id_to_product.tab",header = F,sep = '\t',quote = "")
 colnames(annotation)<-c("tracking_id","product_name")
 
+gene_annotation<-read.table("id_to_gene.tab",header = F,sep = '\t',quote = "")
+colnames(gene_annotation)<-c("tracking_id","gene")
+
 filtered_annotated<-merge(filtered,annotation,by="tracking_id")
+filtered_annotated<-merge(filtered_annotated,gene_annotation,by="tracking_id",all = F)
 filtered_annotated<-filtered_annotated[order(filtered_annotated$sum,decreasing = T),]
+
