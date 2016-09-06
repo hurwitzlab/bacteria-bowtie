241ed7d64666bacf0ca431c18f9765eee73409de
Scott Daniel <scottdaniel25@gmail.com>
2016-09-06 16:10:45 -0700

    it works for mouse

--- scripts/R-interactive/mini-config-mouse.sh
+++ scripts/R-interactive/mini-config-mouse.sh
@@ -4,4 +4,4 @@ echo "Setting this to your working directory of cummerbund data"
 
 export IFS=$'\n'
 
-export RWORK_DIR='/Users/Scott/Google Drive/Hurwitz Lab/cuffdiff_mouse_v1'
+export RWORK_DIR='/Users/Scott/Google Drive/Hurwitz Lab/cuffdiff_topmouse'
