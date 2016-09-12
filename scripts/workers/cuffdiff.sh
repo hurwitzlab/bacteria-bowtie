f5b1a8c91b620cefd9d2602c09fd2d218f646214
Scott G. Daniel <scottdaniel25@gmail.com>
2016-09-12 08:04:27 -0700

    cuffdiff woes

--- scripts/workers/cuffdiff.sh
+++ scripts/workers/cuffdiff.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
 #PBS -W group_list=bhurwitz
-#PBS -q standard
-#PBS -l select=1:ncpus=6:mem=6gb
-#PBS -l cputime=3:00:00
-#PBS -l walltime=3:00:00
+#PBS -q qualified
+###this is probably the memory required for the entire cuffdiff
+###on a 8 million gene set
+#PBS -l select=1:ncpus=12:mem=504gb:pcmem=8gb
+###and the amount of time required to run it
+#PBS -l walltime=180:00:00
+#PBS -l cput=234:00:00
 #PBS -M scottdaniel@email.arizona.edu
 #PBS -m bea
 
@@ -39,4 +42,4 @@ export ALLBAMS="$SAMDIR/allbams"
 
 find $SAMDIR -iname \*.bam -print | sort > $ALLBAMS
 
-time cuffdiff -p 6 -L S1,S2,S3,S4 $GFF $(cat $ALLBAMS) 
+time cuffdiff -p 12 -L S1,S2,S3,S4 $GFF $(cat $ALLBAMS) 
