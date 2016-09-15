bd36d7005b7c8d373a3c58c1a4e403f2534c06fb
Scott Daniel <scottdaniel25@gmail.com>
2016-09-15 11:54:58 -0700

    script to count reads in fastq files

---
+++ scripts/quick_counting_scripts.sh
@@ -0,0 +1,18 @@
+echo STARTED $(date)
+#touch RNA_counts
+#echo "RNA_1" >> RNA_counts
+#find ./ -iname \*RNA_1\* -print0 | xargs -0 -I file egrep '^@.*' file \
+#   | wc -l >> RNA_counts
+echo "Counting reads in RNA_2"
+echo "RNA_2" >> RNA_counts
+find ./ -iname \*RNA_2\* -print0 | xargs -0 -I file egrep '^@.*' file \
+    | wc -l >> RNA_counts
+echo "Counting reads in RNA_3"
+echo "RNA_3" >> RNA_counts
+find ./ -iname \*RNA_3\* -print0 | xargs -0 -I file egrep '^@.*' file \
+    | wc -l >> RNA_counts
+echo "Counting reads in RNA_4"
+echo "RNA_4" >> RNA_counts
+find ./ -iname \*RNA_4\* -print0 | xargs -0 -I file egrep '^@.*' file \
+    | wc -l >> RNA_counts
+echo DONE $(date)
