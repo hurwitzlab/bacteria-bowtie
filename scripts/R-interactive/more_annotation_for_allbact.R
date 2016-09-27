23e11cc73d6c07c426e1473dff95d279d11303e0
Scott Daniel <scottdaniel25@gmail.com>
2016-09-27 10:40:29 -0700

    more annotation script

---
+++ scripts/R-interactive/more_annotation_for_allbact.R
@@ -0,0 +1,6 @@
+setwd("/Users/Scott/Google Drive/Hurwitz Lab/cuffdiff_reallyall")
+
+filtered_annotated <- read.csv("diff_exp_for_all_bact.csv")
+sum_by_product_name <- read.csv("sum_by_product_name.csv")
+sum_by_gene_name <- read.csv("sum_by_gene_name.csv")
+
