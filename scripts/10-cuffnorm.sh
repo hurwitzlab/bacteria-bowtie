3de915636893c0222ba88c097542b07c418efaee
Scott G. Daniel <scottdaniel25@gmail.com>
2016-09-29 14:46:20 -0700

    first try at cuffnorm

--- scripts/10-cuffnorm.sh
+++ scripts/10-cuffnorm.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
 #
-# This script is intended to combine cuffdiff outputs
+# This script is intended to run cuffnorm
 #
 unset module
 set -u
 source ./config.sh
 export CWD="$PWD"
+
+if [[ $# = 0 ]]; then
+    echo "Need to know what sam output directory to work on"
+    echo "e.g. ./07-cuffquant.sh \$MOUSE_OUT"
+    echo "ALSO: did you source config.sh?"
+    exit 1
+fi
+
 
 echo Setting up log files...
 
@@ -14,22 +22,10 @@ PROG=`basename $0 ".sh"`
 #Just going to put stdout and stderr together into stdout
 STDOUT_DIR="$CWD/out/$PROG"
 
-init_dir "$STDOUT_DIR"
+init_dir "$STDOUT_DIR/$(basename $1)"
 
-cd $ALLBACT_OUT
+echo Submitting job...
 
-export GENECOUNTS="isoforms.fpkm_tracking"
+export SAMDIR=$1
 
-set -x
-
-if [[ $(find ./ -iname \*$GENECOUNTS | wc -l) -ge 1 ]]; then
-    find ./ -iname \*$GENECOUNTS -print0 | xargs -0 -I file cat file \
-        > total.fpkm_tracking
-else
-    echo "YOu got the name wrong"
-    echo "Or couldn't find anything"
-    exit 1
-fi
-
-
-
+qsub -V -j oe -o "$STDOUT_DIR/$(basename $1)" $WORKER_DIR/cuffnorm.sh
