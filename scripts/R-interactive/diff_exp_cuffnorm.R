0cb62e3ef1df9a6f5bca19e08948402c51ef0630
Scott Daniel <scottdaniel25@gmail.com>
2016-09-30 12:54:43 -0700

    working

--- scripts/R-interactive/diff_exp_cuffnorm.R
+++ scripts/R-interactive/diff_exp_cuffnorm.R
@@ -1,12 +1,14 @@
 setwd("/Users/Scott/tophat-bacteria/scripts/R-interactive")
 
-system(command = "./process-reallyall.sh")
+system(command = "./process-cuffnorm.sh")
 
-setwd("/Users/Scott/Google Drive/Hurwitz Lab/cuffdiff_reallyall")
+setwd("/Users/Scott/Google Drive/Hurwitz Lab/cuffnorm-out")
 
-simple_gene_counts <- read.table("total.fpkm_tracking",header = T, comment.char = "", strip.white = T, sep = "\t", quote = "", colClasses = c("character","numeric","numeric","numeric","numeric"))
+simple_gene_counts <- read.table("isoforms.FPKM_table",header = T, comment.char = "", strip.white = T, sep = "\t", quote = "", colClasses = c("character","numeric","numeric","numeric","numeric"))
 
-simple_gene_counts$sum<-rowSums(simple_gene_counts[,c("S1_FPKM","S2_FPKM","S3_FPKM","S4_FPKM")])
+colnames(simple_gene_counts)<-c("tracking_id","S1_FPM","S2_FPM","S3_FPM","S4_FPM")
+
+simple_gene_counts$sum<-rowSums(simple_gene_counts[,c("S1_FPM","S2_FPM","S3_FPM","S4_FPM")])
 filtered<-simple_gene_counts[simple_gene_counts$sum!=0,]
 
 annotation<-read.table("id_to_product.tab",header = F,sep = '\t',quote = "",as.is = T)
@@ -21,8 +23,8 @@ filtered_annotated<-merge(filtered,annotation,by="tracking_id")
 filtered_annotated<-merge(filtered_annotated,gene_annotation,by="tracking_id",all = F)
 filtered_annotated<-filtered_annotated[order(filtered_annotated$sum,decreasing = T),]
 
-sum_by_product_name<-rowsum(filtered_annotated[,c("S1_FPKM","S2_FPKM","S3_FPKM","S4_FPKM")],group = filtered_annotated$product_name)
-sum_by_gene_name<-rowsum(filtered_annotated[,c("S1_FPKM","S2_FPKM","S3_FPKM","S4_FPKM")],group = filtered_annotated$gene)
+sum_by_product_name<-rowsum(filtered_annotated[,c("S1_FPM","S2_FPM","S3_FPM","S4_FPM")],group = filtered_annotated$product_name)
+sum_by_gene_name<-rowsum(filtered_annotated[,c("S1_FPM","S2_FPM","S3_FPM","S4_FPM")],group = filtered_annotated$gene)
 
 write.csv(sum_by_product_name,"sum_by_product_name.csv")
 write.csv(sum_by_gene_name,"sum_by_gene_name.csv")
--- scripts/R-interactive/process-cuffnorm.sh
+++ scripts/R-interactive/process-cuffnorm.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-if [[ -e ./mini-config-reallyall.sh ]]; then
-    source ./mini-config-reallyall.sh
+if [[ -e ./mini-config-cuffnorm.sh ]]; then
+    source ./mini-config-cuffnorm.sh
 else
     echo "no config"
     exit 1
@@ -18,13 +18,5 @@ sleep 1
 
 perl -nle '($id)=/ID=([^;]*)/; ($gene)=/gene=([^;]*)/; print "$id\t$gene"' \
     $RWORK_DIR/all-refseq-CDS.gff > $RWORK_DIR/id_to_gene.tab
-
-echo "Removing extra headers"
-cd $RWORK_DIR
-head -1 total.fpkm_tracking > header
-egrep -v '^tracking_id.*' total.fpkm_tracking > temp01
-cat header temp01 > temp02
-cut -f 1,10,14,18,22 temp02 > total.fpkm_tracking
-rm header temp01 temp02
 
 echo "Leaving directory"
