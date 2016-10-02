2fac8c31b6149d26cd78fc12f8ac8ec790780c00
Scott Daniel <scottdaniel25@gmail.com>
2016-10-02 12:48:05 -0700

    angry

--- scripts/R-interactive/more_annotation_forcuffnorm.R
+++ scripts/R-interactive/more_annotation_forcuffnorm.R
@@ -1,10 +1,15 @@
 setwd("/Users/Scott/Google Drive/Hurwitz Lab/cuffnorm-out")
 
+library(KEGGgraph)
+library(KEGGREST)
+
 filtered_annotated <- read.csv("diff_exp_for_all_bact.csv")
 sum_by_product_name <- read.csv("sum_by_product_name.csv")
 colnames(sum_by_product_name)[1]<-"product"
+sum_by_product_name<-sum_by_product_name[,1:5]
 sum_by_gene_name <- read.csv("sum_by_gene_name.csv")
 colnames(sum_by_gene_name)[1]<-"gene"
+sum_by_gene_name<-sum_by_gene_name[,1:5]
 
 #for LPS pathway####
 lps_path<-read.table("LPS_search_list",sep = "\t",comment.char = "#")
@@ -26,6 +31,6 @@ just_poly_genes<-merge(x=sum_by_gene_name,by.x="gene",y=polyamines,by.y="V1",all
 #for m schaedleri####
 mschaedleri <- read.csv("MschaedleriFeatures.csv")
 mschaedleri_exp <- merge(mschaedleri,filtered_annotated,by.x="RefSeq.Locus.Tag",by.y="tracking_id",all=F)
-mschaedleri_exp_vs_all_products <- merge(mschaedleri_exp,sum_by_product_name,by.x="product_name",by.y="X",all=F)
+mschaedleri_exp_vs_all_products <- merge(mschaedleri_exp,sum_by_product_name,by.x="product_name",by.y="product",all=F)
 
 write.csv(mschaedleri_exp_vs_all_products,"mschaedleri_contribution_to_set.csv")
