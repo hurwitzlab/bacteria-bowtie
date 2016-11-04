856918571de8fa39cd33af959a4ec722c324b54d
Scott Daniel <scottdaniel25@gmail.com>
2016-11-04 09:32:05 -0700

    noice

--- scripts/R-interactive/regression analysis and more.R
+++ scripts/R-interactive/regression analysis and more.R
@@ -12,11 +12,11 @@ setwd("/Users/Scott/Google Drive/Hurwitz Lab/cuffnorm-out")
 filtered_annotated <- read.csv("diff_exp_for_all_bact.csv")
 filtered_annotated <- filtered_annotated[,2:9]
 sum_by_product_name <- read.csv("sum_by_product_name.csv")
-colnames(sum_by_product_name)[1]<-"product"
+colnames(sum_by_product_name)<-c("product","s+h+","s-h+","s+h-","s-h-")
 sum_by_product_name<-sum_by_product_name[,1:5]
 sum_by_product_name$product <- tolower(sum_by_product_name$product)
 sum_by_gene_name <- read.csv("sum_by_gene_name.csv")
-colnames(sum_by_gene_name)[1]<-"gene"
+colnames(sum_by_gene_name)<-c("gene","s+h+","s-h+","s+h-","s-h-")
 sum_by_gene_name<-sum_by_gene_name[,1:5]
 sum_by_gene_name$gene <- tolower(sum_by_gene_name$gene)
 
@@ -36,14 +36,55 @@ lps_annot=read.table("LPS_list_annotation",header = T,sep = ";",strip.white = T,
 lps_annot$product <- tolower(lps_annot$product)
 just_lps_products_annot <- merge(x=lps_annot,y=just_lps_products,by="product",all.x=F,all.y=T)
 
-# mouse pcr expression ####
-
+# mouse expression setup ####
 tlr4 <- read.delim("tlr4-pcr-data.tab")
-
 order<-c('s+h-','s-h-','s+h+','s-h+')
-
 tlr4$Series <- factor(tlr4$Series,order)
+nfkb <- read.delim("nfkb-pcr-data.tab")
+nfkb$Series <- factor(nfkb$Series,order)
+tlr2 <- read.delim("tlr2-pcr-data.tab")
+tlr2$Series <- factor(tlr2$Series,order)
 
+boxplot(tlr4$Values~tlr4$Series) -> tlr4box
+boxplot(tlr2$Values~tlr2$Series) -> tlr2box
+boxplot(nfkb$Values~nfkb$Series) -> nfkbbox
+
+#these are the medians from boxplot objects
+tlr4box$stats[3,] -> medians_tlr4
+tlr2box$stats[3,] -> medians_tlr2
+nfkbbox$stats[3,] -> medians_nfkb
+medians_pcr = data.frame(row.names = c("nfkb","tlr2","tlr4"),rbind(medians_nfkb,medians_tlr2,medians_tlr4))
+colnames(medians_pcr) = order
+
+#lets see if these genes correlate with themselves
+i=1
+for (i in i:(length(medians_pcr[,1])-1)) {
+  temp1 = as.matrix(medians_pcr[i,])
+  j = i+1
+  if (i == 1) {
+    for (j in j:(length(medians_pcr[,1]))) {
+      temp2 = as.matrix(medians_pcr[j,])
+      print(paste(row.names(medians_pcr[i,]),"vs.",row.names(medians_pcr[j,])))
+      print(cor.test(temp1,temp2))
+    }
+  }
+  if (i == 2) {
+    temp2 = as.matrix(medians_pcr[3,])
+    print(paste(row.names(medians_pcr[i,]),"vs.",row.names(medians_pcr[j,])))
+    print(cor.test(temp1,temp2))
+  }
+}
+
+#tlr2 correlates with tlr4
+
+#now trying correlation of mouse pcr to bacterial lps gene products
+#first need to remove rows with zeros
+just_lps_products[just_lps_products==0]<-NA
+lpsProductsNoZeroes = na.omit(just_lps_products)
+
+
+
+#old stuff####
 boxplot(tlr4$Values ~ tlr4$Series,col='white')
 
 #S-h- vs. S+h-
@@ -74,7 +115,6 @@ axis(side = 4)
 mtext(side = 4, line = 3, 'B.fragilis scaled counts')
 legend("topleft", legend=c('B.fragilis counts','TLR4 expression'), lty=c(1,0), pch=c(NA, 22), col=c("red", "black"))
 
-#Correlation####
 by_tlr4<-by(tlr4$Values,tlr4[,"Series"],median)
 #by_tlr4<-by(tlr4$Values,tlr4[,"Series"],mean)
 medians_tlr4<-data.frame(series=c('s+h-','s-h-','s+h+','s-h+'),values=c(by_tlr4[[1]],by_tlr4[[2]],by_tlr4[[3]],by_tlr4[[4]]))
@@ -95,15 +135,6 @@ lin_model<-lm(medians_tlr4$values ~ means_lps$value)
 plot(means_lps$values,medians_tlr4$values)
 cor(means_lps$values,medians_tlr4$values)
 cor.test(means_lps$values,medians_tlr4$values)
-
-
-
-#now for nfkb ####
-nfkb <- read.delim("nfkb-pcr-data.tab")
-
-order<-c('s+h-','s-h-','s+h+','s-h+')
-
-nfkb$Series <- factor(nfkb$Series,order)
 
 boxplot(nfkb$Values ~ nfkb$Series,col='white')
 
@@ -139,8 +170,6 @@ clusterof2 <- rbind(nfkb,tlr4)
 clusterof2$Series <- factor(clusterof2$Series,order)
 boxplot(Values ~ Series, data = clusterof2)
 
-
-#Correlation2####
 by_clusterof2<-by(clusterof2$Values,clusterof2[,"Series"],median)
 #by_clusterof2<-by(clusterof2$Values,clusterof2[,"Series"],mean)
 medians_clusterof2<-data.frame(series=c('s+h-','s-h-','s+h+','s-h+'),values=c(by_clusterof2[[1]],by_clusterof2[[2]],by_clusterof2[[3]],by_clusterof2[[4]]))
@@ -152,20 +181,12 @@ plot(medians_clusterof2$values,means_lps$values,ylab='B.fragilis normalized coun
 abline(1620,1323,col='blue')
 legend("bottomright", legend=c('Line of regression','Correlated values', 'pvalue = 0.01459'), lty=c(1,0,0), pch=c(NA, 1,-1), col=c("blue", "black", "black"))
 
-#now for tlr2 ####
-tlr2 <- read.delim("tlr2-pcr-data.tab")
-
-order<-c('s+h-','s-h-','s+h+','s-h+')
-
-tlr2$Series <- factor(tlr2$Series,order)
-
 boxplot(tlr2$Values ~ tlr2$Series,col='white')
 
 clusterof3 <- rbind(nfkb,tlr4,tlr2)
 clusterof3$Series <- factor(clusterof3$Series,order)
 boxplot(Values ~ Series, data = clusterof3)
 
-#Correlation3####
 by_clusterof3<-by(clusterof3$Values,clusterof3[,"Series"],median)
 #by_clusterof3<-by(clusterof3$Values,clusterof3[,"Series"],mean)
 medians_clusterof3<-data.frame(series=c('s+h-','s-h-','s+h+','s-h+'),values=c(by_clusterof3[[1]],by_clusterof3[[2]],by_clusterof3[[3]],by_clusterof3[[4]]))
