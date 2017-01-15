8333dde531e239b624c0076fc320f8bbe4fd5c72
Scott Daniel <scottdaniel25@gmail.com>
2017-01-14 20:27:29 -0700

    memory hog

--- scripts/R-interactive/mini-config-reallyall.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Setting this to your working directory of cummerbund data"
-
-export IFS=$'\n'
-
-export RWORK_DIR='/Users/Scott/Google Drive/Hurwitz Lab/cuffdiff_reallyall'
