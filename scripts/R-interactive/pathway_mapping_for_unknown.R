ab9e18a6c6b523f8a81d15bc6fd52822ea62664a
Scott Daniel <scottdaniel25@gmail.com>
2017-02-05 12:14:24 -0700

    weeeee

--- scripts/R-interactive/pathway_mapping_for_unknown.R
+++ scripts/R-interactive/pathway_mapping_for_unknown.R
@@ -23,9 +23,12 @@ sum_by_product_name$product <- tolower(sum_by_product_name$product)
 
 #now we will attempt to add pathways####
 
-patric_annotation <- read.delim("../combined-cuffnorm-out/product_to_pathway.tab")
+patric_annotation <- read.delim("../unknown-cuffnorm-out/product_to_pathway.tab")
 patric_annotation$product <- tolower(patric_annotation$product)
 with_pathways<-merge(x=sum_by_product_name,y=patric_annotation,by.x="product",by.y="product")
+with_pathways_dups_removed<-with_pathways[!duplicated(with_pathways),]
+with_pathways<-with_pathways_dups_removed
+rm(with_pathways_dups_removed)
 
 ####Start here again####
 #dont want blanks
@@ -43,27 +46,40 @@ shortened<-sum_by_kegg_pathway[sum_by_kegg_pathway$sum>mean(sum_by_kegg_pathway$
 shortened<-shortened[order(shortened$sum , decreasing = T),]
 shortened<-shortened[,c("Name","S3_FPM","S4_FPM","S1_FPM","S2_FPM")]
 colnames(shortened)=c("Name","S+H- (Control)","S-H- (SMAD3 Knockout)","S+H+ (H. hepaticus only)","S-H+ (unknown)")
-
-setwd("~/bacteria-bowtie/scripts/R-interactive/")
-write.table(shortened,"unknown_sum_by_kegg_pathway_above_mean.tab", sep = "\t", quote = T,row.names = F)
-
-shortened<-read.table("unknown_sum_by_kegg_pathway_above_mean.tab",header = T)
 more_shortened<-shortened[1:30,]
-colnames(more_shortened)=c("Name","S+H- (Control)","S-H- (SMAD3 Knockout)","S+H+ (H. hepaticus only)","S-H+ (unknown)")
 
 setwd("~/bacteria-bowtie/scripts/R-interactive/")
-write.table(more_shortened,"unknown_sum_by_kegg_pathway_above_mean.tab", sep = "\t", quote = T,row.names = F)
+write.table(more_shortened,"unknown_sum_by_kegg_pathway.tab", sep = "\t", quote = T,row.names = F)
 
-#Have to run this in an external shell cuz ... perl... grumble
-#system("./bubble.sh unknown_sum_by_kegg_pathway_above_mean.tab unknownBubble")
+system("source ~/.bash_profile && ./bubble.sh unknown_sum_by_kegg_pathway.tab unknownBubble")
 
 system("cp unknownBubble.pdf '/Users/Scott/Google Drive/Hurwitz Lab/manuscripts/'")
 
 more_shortened$unknown_effect<-log(more_shortened$`S-H+ (unknown)`/more_shortened$`S+H- (Control)`)
+more_shortened$Hhep_effect<-log(more_shortened$`S+H+ (H. hepaticus only)`/more_shortened$`S+H- (Control)`)
+more_shortened$smad_effect<-log(more_shortened$`S-H- (SMAD3 Knockout)`/more_shortened$`S+H- (Control)`)
 more_shortened<-more_shortened[order(more_shortened$unknown_effect , decreasing = T),]
 
-#top_sixty_five<-sum_by_kegg_pathway[order(sum_by_kegg_pathway$sum,decreasing = T)[1:65],]
-#top_sixty_five<-top_sixty_five[,c("Name","S1_FPM","S2_FPM","S3_FPM","S4_FPM")]
+#tom's request
+more_shortened$control_effect<-0
 
-#write.table(top_sixty_five,"~/tophat-bacteria/scripts/R-interactive/sum_by_kegg_pathway_top_sixty_five.tab", sep = "\t", quote = T,row.names = F)
+effects <- data.matrix(more_shortened[,6:9])
 
+row.names(effects)<-more_shortened$Name
+
+x=effects
+
+oldPar <- par(no.readonly = T)
+
+myColors=colorRampPalette(c("Blue","Yellow"))
+
+#a heatmap, cuz why not!
+heatmap(x, Rowv=NA, Colv=NA, col = myColors(255),scale="none", margins=c(5,5), cexCol=1, labCol = c("combined", "H. hepaticus","SMAD3-KO","Nothing"))
+
+new_bubble_source <- more_shortened[,1:5]
+
+write.table(new_bubble_source,"sum_by_kegg_pathway_ordered_by_combined_effect.tab", sep = "\t", quote = T,row.names = F)
+
+system("source ~/.bash_profile && ./bubble.sh sum_by_kegg_pathway_ordered_by_combined_effect.tab unknownBubble_orderedbyeffect")
+
+system("cp unknownBubble_orderedbyeffect.pdf '/Users/Scott/Google Drive/Hurwitz Lab/manuscripts/'")
