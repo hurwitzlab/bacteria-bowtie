aa29e34196472d934b829d6ef82cb3f2f709b3f1
Scott Daniel <scottdaniel25@gmail.com>
2017-02-15 09:19:45 -0700

    benzoate degradation pathway

--- scripts/R-interactive/other_pathway_mapping_for_cuffnorm.R
+++ scripts/R-interactive/other_pathway_mapping_for_cuffnorm.R
@@ -1,3 +1,10 @@
+setwd("/Users/Scott/Google Drive/Hurwitz Lab/cuffnorm-out")
+
+library(reshape2)
+library(tidyr)
+library(RColorBrewer)
+library(xlsx)
+
 #setup####
 with_pathways<-read.table("with_pathways.tab",header=T)
 
@@ -26,15 +33,40 @@ oxphos_product_sums<-oxphos_product_sums[order(oxphos_product_sums$combined_effe
 #welp, the top changed product is atp synthase, both positive and negative! ha!
 
 #what are the actual species for atpb
-genome_to_feature <- read.delim("genome-name_to_refseq-locus-tag")
-
-atpb<-with_pathways[product_name=="atp synthase beta chain" & pathway=="00190|Oxidative phosphorylation",]
-important <- merge(x=atpb,y=genome_to_feature,by.x="tracking_id",by.y="refseq_locus_tag",all = F)
-#and for the lowest f0f1 atp synthase subunit alpha
-atpa<-with_pathways[product_name=="atp synthase alpha chain" & pathway=="00190|Oxidative phosphorylation",]
-important2 <- merge(x=atpa,y=genome_to_feature,by.x="tracking_id",by.y="refseq_locus_tag",all = F)
+# genome_to_feature <- read.delim("genome-name_to_refseq-locus-tag")
+#
+# atpb<-with_pathways[product_name=="atp synthase beta chain" & pathway=="00190|Oxidative phosphorylation",]
+# important <- merge(x=atpb,y=genome_to_feature,by.x="tracking_id",by.y="refseq_locus_tag",all = F)
+# #and for the lowest f0f1 atp synthase subunit alpha
+# atpa<-with_pathways[product_name=="atp synthase alpha chain" & pathway=="00190|Oxidative phosphorylation",]
+# important2 <- merge(x=atpa,y=genome_to_feature,by.x="tracking_id",by.y="refseq_locus_tag",all = F)
 
 #lets write some excel tables
-write.xlsx(oxphos_product_sums,"oxphos_product_sums.xlsx")
-write.xlsx(important,"atp synthase beta chain downregulated.xlsx")
-write.xlsx(important2,"atp synthatse alpha chain upregualted.xlsx")
+# write.xlsx(oxphos_product_sums,"oxphos_product_sums.xlsx")
+# write.xlsx(important,"atp synthase beta chain downregulated.xlsx")
+# write.xlsx(important2,"atp synthatse alpha chain upregualted.xlsx")
+
+# benzoate degredation ####
+
+benzoate<-with_pathways[pathway=="00362|Benzoate degradation via hydroxylation",]
+no_blanks<-benzoate[benzoate$product_name!="",]
+benzoate<-no_blanks
+rm(no_blanks)
+
+benzoate_product_sums<-rowsum(benzoate[,c("S3_FPM","S4_FPM","S1_FPM","S2_FPM")],group=benzoate$product_name)
+truthy<-apply(benzoate_product_sums,1,function(row) all(row != 0))
+no_zeros<-benzoate_product_sums[truthy,]
+benzoate_product_sums<-no_zeros
+rm(truthy,no_zeros)
+
+colnames(benzoate_product_sums)=c("S+H- (Control)","S-H- (SMAD3 Knockout)","S+H+ (H. hepaticus only)","S-H+ (Combined)")
+
+benzoate_product_sums$combined_effect<-log(benzoate_product_sums$`S-H+ (Combined)`/benzoate_product_sums$`S+H- (Control)`)
+benzoate_product_sums$Hhep_effect<-log(benzoate_product_sums$`S+H+ (H. hepaticus only)`/benzoate_product_sums$`S+H- (Control)`)
+benzoate_product_sums$smad_effect<-log(benzoate_product_sums$`S-H- (SMAD3 Knockout)`/benzoate_product_sums$`S+H- (Control)`)
+benzoate_product_sums<-benzoate_product_sums[order(benzoate_product_sums$combined_effect , decreasing = T),]
+
+
+
+#lets write some excel tables
+write.xlsx(benzoate_product_sums,"benzoate_product_sums.xlsx")
