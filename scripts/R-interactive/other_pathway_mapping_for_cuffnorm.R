9424ea2f5b8dc904ca4e2b450b858cfc3d4468d2
Scott Daniel <scottdaniel25@gmail.com>
2017-02-16 15:18:30 -0700

    going forward

--- scripts/R-interactive/other_pathway_mapping_for_cuffnorm.R
+++ scripts/R-interactive/other_pathway_mapping_for_cuffnorm.R
@@ -7,6 +7,8 @@ library(plyr)
 #setup####
 with_pathways<-read.table("with_pathways.tab",header=T)
 
+#PATHWAYS THAT ARE UP OVERALL####
+
 #oxidative phosphorylation####
 
 attach(with_pathways)
@@ -159,5 +161,128 @@ citrate_ec_sums<-citrate_ec_sums[order(citrate_ec_sums$combined_effect , decreas
 
 #lets write some excel tables
 write.xlsx(citrate_ec_sums,"citrate_ec_sums_with_aliases.xlsx",row.names = F)
+
+# alanine, aspartate and glutamate metabolism ####
+
+alanine<-with_pathways[pathway=="00250|Alanine, aspartate and glutamate metabolism",]
+no_blanks<-alanine[alanine$product_name!="",]
+alanine<-no_blanks
+rm(no_blanks)
+
+alanine_ec_sums<-rowsum(alanine[,c("S3_FPM","S4_FPM","S1_FPM","S2_FPM")],group=alanine$ec_number)
+truthy<-apply(alanine_ec_sums,1,function(row) all(row != 0))
+no_zeros<-alanine_ec_sums[truthy,]
+alanine_ec_sums<-no_zeros
+rm(truthy,no_zeros)
+
+colnames(alanine_ec_sums)=c("S+H- (Control)","S-H- (SMAD3 Knockout)","S+H+ (H. hepaticus only)","S-H+ (Combined)")
+alanine_ec_sums$ec_number=row.names(alanine_ec_sums)
+
+#decorate ec_sums with gene and product names
+alanine_ec_sums <- merge(alanine_ec_sums,alanine[,c("product_name","gene","ec_number")],by="ec_number",all=F)
+no_dups<-alanine_ec_sums[!duplicated(alanine_ec_sums),]
+alanine_ec_sums<-no_dups
+rm(no_dups)
+
+#combine gene and product_name columns
+combined<-unite(alanine_ec_sums,aliases,c(product_name,gene),sep = ",")
+alanine_ec_sums<-combined
+rm(combined)
+
+#grab together the multiple product names
+collapsed<-ddply(alanine_ec_sums, .(ec_number,`S+H- (Control)`,`S-H- (SMAD3 Knockout)`,`S+H+ (H. hepaticus only)`,`S-H+ (Combined)`), summarize, aliases=toString(aliases))
+alanine_ec_sums<-collapsed
+rm(collapsed)
+
+alanine_ec_sums$combined_effect<-log(alanine_ec_sums$`S-H+ (Combined)`/alanine_ec_sums$`S+H- (Control)`)
+alanine_ec_sums$Hhep_effect<-log(alanine_ec_sums$`S+H+ (H. hepaticus only)`/alanine_ec_sums$`S+H- (Control)`)
+alanine_ec_sums$smad_effect<-log(alanine_ec_sums$`S-H- (SMAD3 Knockout)`/alanine_ec_sums$`S+H- (Control)`)
+alanine_ec_sums<-alanine_ec_sums[order(alanine_ec_sums$combined_effect , decreasing = T),]
+
+#lets write some excel tables
+write.xlsx(alanine_ec_sums,"alanine_ec_sums_with_aliases.xlsx",row.names = F)
+
+# 00630|Glyoxylate and dicarboxylate metabolism ####
+
+glyoxylate<-with_pathways[pathway=="00630|Glyoxylate and dicarboxylate metabolism",]
+no_blanks<-glyoxylate[glyoxylate$product_name!="",]
+glyoxylate<-no_blanks
+rm(no_blanks)
+
+glyoxylate_ec_sums<-rowsum(glyoxylate[,c("S3_FPM","S4_FPM","S1_FPM","S2_FPM")],group=glyoxylate$ec_number)
+truthy<-apply(glyoxylate_ec_sums,1,function(row) all(row != 0))
+no_zeros<-glyoxylate_ec_sums[truthy,]
+glyoxylate_ec_sums<-no_zeros
+rm(truthy,no_zeros)
+
+colnames(glyoxylate_ec_sums)=c("S+H- (Control)","S-H- (SMAD3 Knockout)","S+H+ (H. hepaticus only)","S-H+ (Combined)")
+glyoxylate_ec_sums$ec_number=row.names(glyoxylate_ec_sums)
+
+#decorate ec_sums with gene and product names
+glyoxylate_ec_sums <- merge(glyoxylate_ec_sums,glyoxylate[,c("product_name","gene","ec_number")],by="ec_number",all=F)
+no_dups<-glyoxylate_ec_sums[!duplicated(glyoxylate_ec_sums),]
+glyoxylate_ec_sums<-no_dups
+rm(no_dups)
+
+#combine gene and product_name columns
+combined<-unite(glyoxylate_ec_sums,aliases,c(product_name,gene),sep = ",")
+glyoxylate_ec_sums<-combined
+rm(combined)
+
+#grab together the multiple product names
+collapsed<-ddply(glyoxylate_ec_sums, .(ec_number,`S+H- (Control)`,`S-H- (SMAD3 Knockout)`,`S+H+ (H. hepaticus only)`,`S-H+ (Combined)`), summarize, aliases=toString(aliases))
+glyoxylate_ec_sums<-collapsed
+rm(collapsed)
+
+glyoxylate_ec_sums$combined_effect<-log(glyoxylate_ec_sums$`S-H+ (Combined)`/glyoxylate_ec_sums$`S+H- (Control)`)
+glyoxylate_ec_sums$Hhep_effect<-log(glyoxylate_ec_sums$`S+H+ (H. hepaticus only)`/glyoxylate_ec_sums$`S+H- (Control)`)
+glyoxylate_ec_sums$smad_effect<-log(glyoxylate_ec_sums$`S-H- (SMAD3 Knockout)`/glyoxylate_ec_sums$`S+H- (Control)`)
+glyoxylate_ec_sums<-glyoxylate_ec_sums[order(glyoxylate_ec_sums$combined_effect , decreasing = T),]
+
+#lets write some excel tables
+write.xlsx(glyoxylate_ec_sums,"glyoxylate_ec_sums_with_aliases.xlsx",row.names = F)
+
+# 00680|Methane metabolism ####
+
+methane<-with_pathways[pathway=="00680|Methane metabolism",]
+no_blanks<-methane[methane$product_name!="",]
+methane<-no_blanks
+rm(no_blanks)
+
+methane_ec_sums<-rowsum(methane[,c("S3_FPM","S4_FPM","S1_FPM","S2_FPM")],group=methane$ec_number)
+truthy<-apply(methane_ec_sums,1,function(row) all(row != 0))
+no_zeros<-methane_ec_sums[truthy,]
+methane_ec_sums<-no_zeros
+rm(truthy,no_zeros)
+
+colnames(methane_ec_sums)=c("S+H- (Control)","S-H- (SMAD3 Knockout)","S+H+ (H. hepaticus only)","S-H+ (Combined)")
+methane_ec_sums$ec_number=row.names(methane_ec_sums)
+
+#decorate ec_sums with gene and product names
+methane_ec_sums <- merge(methane_ec_sums,methane[,c("product_name","gene","ec_number")],by="ec_number",all=F)
+no_dups<-methane_ec_sums[!duplicated(methane_ec_sums),]
+methane_ec_sums<-no_dups
+rm(no_dups)
+
+#combine gene and product_name columns
+combined<-unite(methane_ec_sums,aliases,c(product_name,gene),sep = ",")
+methane_ec_sums<-combined
+rm(combined)
+
+#grab together the multiple product names
+collapsed<-ddply(methane_ec_sums, .(ec_number,`S+H- (Control)`,`S-H- (SMAD3 Knockout)`,`S+H+ (H. hepaticus only)`,`S-H+ (Combined)`), summarize, aliases=toString(aliases))
+methane_ec_sums<-collapsed
+rm(collapsed)
+
+methane_ec_sums$combined_effect<-log(methane_ec_sums$`S-H+ (Combined)`/methane_ec_sums$`S+H- (Control)`)
+methane_ec_sums$Hhep_effect<-log(methane_ec_sums$`S+H+ (H. hepaticus only)`/methane_ec_sums$`S+H- (Control)`)
+methane_ec_sums$smad_effect<-log(methane_ec_sums$`S-H- (SMAD3 Knockout)`/methane_ec_sums$`S+H- (Control)`)
+methane_ec_sums<-methane_ec_sums[order(methane_ec_sums$combined_effect , decreasing = T),]
+
+#lets write some excel tables
+write.xlsx(methane_ec_sums,"methane_ec_sums_with_aliases.xlsx",row.names = F)
+
+#PATHWAYS THAT ARE DOWN OVERALL####
+
 
 
