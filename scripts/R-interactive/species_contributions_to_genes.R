88aab3e106292865d5073273a2627fdbddf3f65f
Scott Daniel <scottdaniel25@gmail.com>
2017-02-27 16:50:53 -0700

    going back god damn you git wtf

--- scripts/R-interactive/species_contributions_to_genes.R
+++ scripts/R-interactive/species_contributions_to_genes.R
@@ -1,4 +1,4 @@
-setwd("/Users/Scott/Google Drive/Hurwitz Lab/combined-cuffnorm-out")
+setwd("/Users/Scott/cuffnorm-out")
 
 library(RColorBrewer)
 library(reshape2)
@@ -48,41 +48,43 @@ poly_annot$product <- tolower(poly_annot$product)
 just_poly_products_annot <- merge(x=poly_annot,y=just_poly_products)
 attach(just_poly_products_annot)
 
-#important<-just_poly_products_annot[ecnumber %in% c("[EC:3.5.1.53]","[EC:4.1.1.96]"),]
+important<-just_poly_products_annot[ecnumber %in% c("[EC:3.5.1.53]","[EC:4.1.1.96]"),]
 #doing those three that are in a row before putrescine
-important<-just_poly_products_annot[ecnumber %in% c("[EC:4.1.1.19]","[EC:3.5.3.12]","[EC:3.5.1.53]"),]
+#important<-just_poly_products_annot[ecnumber %in% c("[EC:4.1.1.19]","[EC:3.5.3.12]","[EC:3.5.1.53]"),]
 
 
-#important2 <- merge(x=important[,1:4],y=filtered_annotated,by.x="product",by.y="product_name",all.x=T)
+important2 <- merge(x=important[,1:4],y=filtered_annotated,by.x="product",by.y="product_name",all.x=T)
+important <- merge(x=important2[,c(4:9)],y=genome_to_feature,by.x="tracking_id",by.y="refseq_locus_tag",all.x=T)
+
 #need to do this with grep because names aren't exact
-two<-filtered_annotated[grep('agmatine deiminase',filtered_annotated$product_name,perl=T),]
-three<-filtered_annotated[grep('n-carbamoylputrescine amidase',filtered_annotated$product_name,perl=T),]
-one<-filtered_annotated[grep('arginine decarboxylase',filtered_annotated$product_name,perl=T),]
-four<-rbind(one,two,three)
-important <- merge(x=four[,c(1:5,6,7)],y=genome_to_feature,by.x="tracking_id",by.y="refseq_locus_tag",all.x=T)
-important$genome_name <- lapply(important$genome_name, as.character)
-important[is.na(important)]<-"unknown"
+# two<-filtered_annotated[grep('agmatine deiminase',filtered_annotated$product_name,perl=T),]
+# three<-filtered_annotated[grep('n-carbamoylputrescine amidase',filtered_annotated$product_name,perl=T),]
+# one<-filtered_annotated[grep('arginine decarboxylase',filtered_annotated$product_name,perl=T),]
+# four<-rbind(one,two,three)
+# important <- merge(x=four[,c(1:5,6,7)],y=genome_to_feature,by.x="tracking_id",by.y="refseq_locus_tag",all.x=T)
+#important2$genome_name <- lapply(important$genome_name, as.character)
+#important[is.na(important)]<-"unknown"
 
-colnames(important)[2:5]<-c("S+H+ (H. hepaticus only)","S-H+ (Combined)","S+H- (Control)","S-H- (SMAD3 Knockout)")
+colnames(important)[3:6]<-c("S+H+ (H. hepaticus only)","S-H+ (Combined)","S+H- (Control)","S-H- (SMAD3 Knockout)")
 
 #cut out all the unnecessary stuff so they group appropriately
-important$product_name<-gsub('.*arginine decarboxylase.*','arginine decarboxylase',important$product_name,perl = T)
-important$product_name<-gsub('.*agmatine deiminase.*','agmatine deiminase',important$product_name,perl = T)
-important$product_name<-gsub('.*n-carbamoylputrescine amidase.*','n-carbamoylputrescine amidase',important$product_name,perl = T)
+# important$product_name<-gsub('.*arginine decarboxylase.*','arginine decarboxylase',important$product_name,perl = T)
+# important$product_name<-gsub('.*agmatine deiminase.*','agmatine deiminase',important$product_name,perl = T)
+# important$product_name<-gsub('.*n-carbamoylputrescine amidase.*','n-carbamoylputrescine amidase',important$product_name,perl = T)
 
 important$genome_name<-as.character(important$genome_name)
-melted<-melt(important[,c(2:5,7,8)])
+melted<-melt(important[,c(2:7)])
 order<-c("S+H- (Control)","S-H- (SMAD3 Knockout)","S+H+ (H. hepaticus only)","S-H+ (Combined)")
 melted <- melted %>% mutate(variable =  factor(variable, levels = order)) %>% arrange(variable)
-colnames(melted)<-c("product_name","genome","Sample","count")
+colnames(melted)<-c("ecnumber","genome","Sample","RNA count")
 
-melted <- with(melted, melted[order(product_name, genome, Sample),])
-plot.bar = ggplot(data=melted, aes(x=Sample, y=count, fill=genome))
-plot.bar + geom_bar(stat="identity", col="black", size = .5) + facet_grid(~product_name) + theme(text = element_text(size=12)) + theme(axis.text.x = element_text(angle = 45, hjust = 1)) #+ guides(fill=FALSE)
-plot.bar + geom_bar(stat="identity", col="black", size = .5) + facet_grid(~product_name) + theme(text = element_text(size=12)) + theme(axis.text.x = element_text(angle = 45, hjust = 1)) + guides(fill=FALSE)
+melted <- with(melted, melted[order(ecnumber, genome, Sample),])
+plot.bar = ggplot(data=melted, aes(x=Sample, y=`RNA count`, fill=genome))
+plot.bar + geom_bar(stat="identity", col="black", size = .5) + facet_grid(~ecnumber) + theme(text = element_text(size=12)) + theme(axis.text.x = element_text(angle = 45, hjust = 1)) #+ guides(fill=FALSE)
+plot.bar + geom_bar(stat="identity", col="black", size = .5) + facet_grid(~ecnumber) + theme(text = element_text(size=12)) + theme(axis.text.x = element_text(angle = 45, hjust = 1)) + guides(fill=FALSE)
 
 #interactive
-plot.bar <- ggplot(data=melted, aes(x=Sample, y=count, fill=genome, tooltip = genome, data_id = Sample)) + geom_bar_interactive(stat="identity", col="black", size = .5) + facet_grid(~product_name) + theme(text = element_text(size=12)) + theme(axis.text.x = element_text(angle = 45, hjust = 1)) + guides(fill=FALSE)
+plot.bar <- ggplot(data=melted, aes(x=Sample, y=`RNA count`, fill=genome, tooltip = genome, data_id = Sample)) + geom_bar_interactive(stat="identity", col="black", size = .5) + facet_grid(~ecnumber) + theme(text = element_text(size=12)) + theme(axis.text.x = element_text(angle = 45, hjust = 1)) + guides(fill=FALSE)
 
 ggiraph(code = print(plot.bar))
 detach(just_poly_products_annot)
@@ -100,14 +102,14 @@ just_butanoate_products_annot <- merge(x=butanoate_annot,y=just_butanoate_produc
 attach(just_butanoate_products_annot)
 
 important<-just_butanoate_products_annot[ecnumber %in% c("[EC:2.7.2.7]","[EC:2.3.1.19]"),]
-#important2 <- merge(x=important[,1:4],y=filtered_annotated,by.x="product",by.y="product_name",all.x=T)
+important2 <- merge(x=important[,1:4],y=filtered_annotated,by.x="product",by.y="product_name",all.x=T)
 #need to do this with grep because names aren't exact
-one<-filtered_annotated[grep('butyrate kinase',filtered_annotated$product_name,perl=T),]
-two<-filtered_annotated[grep('phosphate butyryltransferase',filtered_annotated$product_name,perl=T),]
-three<-rbind(one,two)
-important <- merge(x=three[,c(1:5,6,7)],y=genome_to_feature,by.x="tracking_id",by.y="refseq_locus_tag",all.x=T)
+# one<-filtered_annotated[grep('butyrate kinase',filtered_annotated$product_name,perl=T),]
+# two<-filtered_annotated[grep('phosphate butyryltransferase',filtered_annotated$product_name,perl=T),]
+# three<-rbind(one,two)
+important <- merge(x=important2[,c(1:5,6,7)],y=genome_to_feature,by.x="tracking_id",by.y="refseq_locus_tag",all.x=T)
 important$genome_name <- lapply(important$genome_name, as.character)
-important[is.na(important)]<-"unknown"
+#important[is.na(important)]<-"unknown"
 
 
 colnames(important)[2:5]<-c("S+H+ (H. hepaticus only)","S-H+ (Combined)","S+H- (Control)","S-H- (SMAD3 Knockout)")
